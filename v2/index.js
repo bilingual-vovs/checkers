060c9e6ddf300e2479044535215d183de3d8bd02
bilingual-vovs <vovavovsart@gmail.com>
2022-05-08 11:41:23 +0300

    Adding posibility of adding checkers

--- v2/index.js
+++ v2/index.js
@@ -24,25 +24,43 @@ const patterns = {
 
 class Desk{
     constructor(size, resolution, position, numOfLines, style){
-        this.object = {
-            size: size,
-            resolution: resolution,
-            position: position,
-            numOfLines:numOfLines,
-            style: style
-        }
+        this.size = size
+        this.resolution = resolution
+        this.position = position
+        this.numOfLines = numOfLines
 
         document.body.innerHTML += patterns.desk.backgorund('background-01', size, position, style)
-
         
         let color = false
         let step = size/resolution
+        this.cells = []
+        let whiteCheckersCount = 0
+        let blackCheckersCount = 0
 
         for (let a = 0; a<resolution;a++){
             for(let b = 0; b<resolution;b++){
-                document.getElementById('background-01').innerHTML += color ? patterns.desk.blackCell(`a-${a}-b${b}`, step, {x: a*step, y: b*step}):patterns.desk.whiteCell(`a-${a}-b${b}`, step, {x: a*step, y: b*step})
+                document.getElementById('background-01').innerHTML += color ? patterns.desk.blackCell(this.cells.length, step, {x: a*step, y: b*step}):patterns.desk.whiteCell(this.cells.length, step, {x: a*step, y: b*step})
+                
+                this.cells.push({
+                    a: a,
+                    b: b, 
+                    checker: (color && (b<numOfLines || resolution-numOfLines<=b)) ? this.createChecker(`white-${whiteCheckersCount}`, {a: a,b: b}, step, (b<=numOfLines) ? true:false, this.cells.length): "none"
+                })
+
                 color = (resolution%2==0&&b==7) ? color:!color
             }
         }
+    }
+    createChecker(id, position, sizeOfCell, color, cellId, style){
+        document.getElementById(cellId).innerHTML = color ? patterns.checkers.white(id, sizeOfCell, style):patterns.checkers.black(id, sizeOfCell, style)
+        return {
+            id: id,
+            position: position,
+            cellId: cellId,
+            sizeOfCell: sizeOfCell,
+            isLady: false,
+            style: style
+        }
+        
     }
 }
